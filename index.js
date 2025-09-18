07e91b0e8defe604bf73c63428bc2087c486677b
Samsku <turunensami54@gmail.com>
2025-09-18 21:07:45 +0300

    File analyze logic

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 
-var app = express();
+require("dotenv").config();
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  if (!req.file) return res.status(400).send("No file uploaded");
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
